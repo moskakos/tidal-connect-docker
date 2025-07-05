780c18cc2c6683f1dd66fed6d965c918ce435fe7
moskakos <juha.koskimaki@iki.fi>
2025-07-05 16:07:35 +0300

    translate comments to English

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -e
 
-# Defaultit
+# Defaults
 TC_DEVICE="${TC_DEVICE:-plughw:0,0}"
 SNAPCLIENT_DEVICE="${SNAPCLIENT_DEVICE:-plughw:0,1}"
 SNAPSERVER_HOST="${SNAPSERVER_HOST:-127.0.0.1}"
 
-echo "===> Käynnistetään Speaker Controller (tmux taustalla)"
+echo "===> Starting Speaker Controller in tmux session"
 tmux new-session -d -s speaker_controller_application '/app/ifi-tidal-release/bin/speaker_controller_application'
 
-echo "===> Käynnistetään Tidal Connect ($TC_DEVICE)"
+echo "===> Starting Tidal Connect ($TC_DEVICE)"
 /app/ifi-tidal-release/bin/tidal_connect_application \
    --tc-certificate-path "/app/ifi-tidal-release/id_certificate/IfiAudio_ZenStream.dat" \
-   --playback-device "${TC_DEVICE}" \
+   --playback-device "${TC_DEVICE:-plughw:0,0}" \
    -f "Tidal Connect (Docker)" \
    --codec-mpegh true \
    --codec-mqa false \
@@ -30,7 +30,7 @@ snapclient -h "${SNAPSERVER_HOST}" --player alsa --device "${SNAPCLIENT_DEVICE}"
 
 PID_SNAP=$!
 
-# Odotetaan molempien prosessien valmistumista
+# Wait for both processes to finish
 wait $PID_TIDAL $PID_SNAP
 
-echo "===> TIDAL Connect Container sammui."
+echo "===> TIDAL Connect Container stopped."
