57f4cefc0cdfe594fcb22646b53d355adebcd01e
moskakos <juha.koskimaki@iki.fi>
2025-07-07 00:08:34 +0300

    add function for removing stream from snapserver

--- ffmpeg/entrypoint.sh
+++ ffmpeg/entrypoint.sh
@@ -17,9 +17,41 @@ error() {
   exit 1
 }
 
+# Function to remove stream from Snapserver
+remove_stream() {
+  info "Removing stream from Snapserver"
+  curl -s -X POST http://${SNAPSERVER_HOST}:${SNAPSERVER_API_PORT}/jsonrpc \
+    -H 'Content-Type: application/json' \
+    -d "{\"id\":1, \"jsonrpc\":\"2.0\", \"method\":\"Stream.RemoveStream\", \"params\":{\"id\":\"${STREAM_NAME}\"}}"
+  
+  if [ $? -ne 0 ]; then
+    warning "Failed to remove stream from Snapserver."
+  else
+    info "Stream removed successfully"
+  fi
+}
+
+# Signal handler function
+cleanup() {
+  info "Container stopping, cleaning up..."
+  # Kill ffmpeg if it's running
+  if [ ! -z "$FFMPEG_PID" ] && kill -0 $FFMPEG_PID 2>/dev/null; then
+    info "Stopping ffmpeg process"
+    kill $FFMPEG_PID
+    wait $FFMPEG_PID 2>/dev/null
+  fi
+  
+  # Remove stream from snapserver
+  remove_stream
+  exit 0
+}
+
+# Set up signal trap
+trap cleanup SIGTERM SIGINT
+
 # Set defaults if not provided in environment
 SNAPSERVER_HOST=${SNAPSERVER_HOST:-snapcast}
-SNAPSERVER_API_PORT=${SNAPSERVER_API_PORT:-1780} # API port for Snapserver
+SNAPSERVER_API_PORT=${SNAPSERVER_API_PORT:-1780}
 STREAM_PORT=${STREAM_PORT:-5000}
 STREAM_NAME=${STREAM_NAME:-Tidal}
 AUDIO_FORMAT=${AUDIO_FORMAT:-flac}
@@ -27,6 +59,8 @@ AUDIO_DEVICE=${AUDIO_DEVICE:-plughw:Loopback,0}
 SAMPLE_RATE=${SAMPLE_RATE:-44100}
 CHANNELS=${CHANNELS:-2}
 BUFFER_SIZE=${BUFFER_SIZE:-1024}
+AUDIO_BUFFER=${AUDIO_BUFFER:-2048}
+AUDIO_QUALITY=${AUDIO_QUALITY:-5}
 
 info "Starting ffmpeg audio forwarder for Snapserver"
 info "Snapserver API: $SNAPSERVER_HOST:$SNAPSERVER_API_PORT"
@@ -46,10 +80,31 @@ if [ $? -ne 0 ]; then
   warning "Check that Snapserver is running on ${SNAPSERVER_HOST}:${SNAPSERVER_API_PORT}"
 fi
 
-# Start audio forwarding
-info "Starting audio forwarding"
-exec ffmpeg -hide_banner -loglevel info \
-  -f alsa -ac ${CHANNELS} -ar ${SAMPLE_RATE} -i ${AUDIO_DEVICE} \
+# Start audio forwarding (as background process)
+info "Starting audio forwarding with enhanced quality"
+ffmpeg -hide_banner -loglevel info \
+  -f alsa \
+  -thread_queue_size 4096 \
+  -ac ${CHANNELS} \
+  -ar ${SAMPLE_RATE} \
+  -i ${AUDIO_DEVICE} \
   -buffer_size ${BUFFER_SIZE} \
-  -c:a ${AUDIO_FORMAT} -f ${AUDIO_FORMAT} \
-  tcp://${SNAPSERVER_HOST}:${STREAM_PORT}
+  -af "aresample=async=1000:min_hard_comp=0.01:first_pts=0" \
+  -c:a ${AUDIO_FORMAT} \
+  -compression_level ${AUDIO_QUALITY} \
+  -frame_size ${AUDIO_BUFFER} \
+  -application audio \
+  -fflags nobuffer \
+  -flags low_delay \
+  -max_delay 500000 \
+  -f ${AUDIO_FORMAT} \
+  tcp://${SNAPSERVER_HOST}:${STREAM_PORT} &
+
+# Save ffmpeg PID so we can terminate it properly
+FFMPEG_PID=$!
+
+# Wait for ffmpeg to exit
+wait $FFMPEG_PID
+
+# If ffmpeg exits on its own, also clean up
+cleanup
