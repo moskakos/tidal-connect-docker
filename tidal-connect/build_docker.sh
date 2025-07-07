e4890247e422ca31264804cfa87494aa2159a69e
moskakos <juha.koskimaki@iki.fi>
2025-07-07 20:27:23 +0300

    update build process

--- tidal-connect/build_docker.sh
+++ tidal-connect/build_docker.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+# tidal-connect/build_docker.sh
 
-DOCKER_IMAGE=${DOCKER_IMAGE:-moskakos/tidal-connect}
+DOCKER_HOST=${DOCKER_HOST:-"tidal"}
+CONTAINER_NAME=${CONTAINER_NAME:-"moskakos/tidal-connect"}
 
-echo "Building Docker image: ${DOCKER_IMAGE}"
-cd .. &&  docker build -f Docker/Dockerfile -t ${DOCKER_IMAGE} .
-echo "Done..."
+# SSH into the Docker host and stop & remove the container
+ssh "$DOCKER_HOST" "docker stop $CONTAINER_NAME || true && docker rm $CONTAINER_NAME || true"
+
+# Build the Docker image and transfer it to the Docker host
+docker build --no-cache -f Dockerfile -t "$CONTAINER_NAME" .
+docker image save "$CONTAINER_NAME" | pv | ssh "$DOCKER_HOST" "docker load"
